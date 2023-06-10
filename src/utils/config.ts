14aa45e0e8b79c98c51947bb0edfbfb122ff0730
ltfei <ltfei@ltfei.cn>
2023-06-10 14:01:26 +0800

    feat(config): 获取package.json信息

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -1,10 +1,19 @@
 import localConfig from './localConfig'
 import { Config } from '../types/config'
+import { getPackage } from './package'
 
 export const getConfig = async <T extends keyof Config, K extends keyof Config[T]>(
   type: T,
   key: K
 ) => {
+  // 系统级配置 通过系统获取 无法通过配置文件修改
+  if (type == 'system') {
+    if (type == 'system' && key == 'version') {
+      const { version } = await getPackage()
+      return version
+    }
+    return null
+  }
   return localConfig[type][key]
 }
 
