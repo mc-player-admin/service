35b68c037105741ed56ecd1e5d41e58526ab0a78
ltfei <ltfei@ltfei.cn>
2023-06-23 15:39:23 +0800

    feat(Audit/submit): 提交审核

--- src/data/sql.ts
+++ src/data/sql.ts
@@ -292,6 +292,134 @@ create table users
 );
 `
     ]
+  },
+  {
+    sqlVersion: 6,
+    update: [
+      `create table audits
+      (
+          id            int          not null
+              primary key,
+          user          int          not null,
+          name          varchar(50)  not null,
+          bili_username varchar(50)  not null,
+          bili_uid      varchar(50)  not null,
+          screenshot    varchar(255) not null,
+          status        int          not null,
+          result        int          null,
+          cause         text         null,
+          approver      int          null,
+          date          datetime     not null,
+          constraint audits_pk
+              unique (id)
+      );
+      
+      `
+    ],
+    init: [
+      `create table audits
+(
+    id            int          not null
+        primary key,
+    user          int          not null,
+    name          varchar(50)  not null,
+    bili_username varchar(50)  not null,
+    bili_uid      varchar(50)  not null,
+    screenshot    varchar(255) not null,
+    status        int          not null,
+    result        int          null,
+    cause         text         null,
+    approver      int          null,
+    date          datetime     not null,
+    constraint audits_pk
+        unique (id)
+);`,
+      `
+
+create table config
+(
+    id             int auto_increment
+        primary key,
+    name           varchar(100) not null,
+    value          text         not null,
+    last_edit_date datetime     null
+);`,
+      `
+
+create table login_queue
+(
+    id         int auto_increment
+        primary key,
+    uuid       varchar(100) not null,
+    date       datetime     not null,
+    status     int          not null,
+    user       int          null,
+    ip         varchar(255) null,
+    user_agent varchar(255) null,
+    constraint login_queue_pk
+        unique (uuid),
+    constraint login_queue_pk3
+        unique (id)
+);`,
+      `
+
+create table old_players_1
+(
+    id            int         null,
+    qq            varchar(20) null,
+    name          varchar(30) null,
+    status        int         null,
+    date          datetime    null,
+    join_type     int         null,
+    score         int         null,
+    xuid          varchar(20) null,
+    credit_points int         null
+);`,
+      `
+
+create table old_players_2
+(
+    id         int          null,
+    name       varchar(100) null,
+    date       datetime     null,
+    user_id    int          null,
+    status     int          null,
+    qq         varchar(20)  null,
+    unban_date datetime     null
+);`,
+      `
+
+create table players
+(
+    id          int auto_increment
+        primary key,
+    name        varchar(50) not null,
+    transfer_id int         null,
+    user        int         not null,
+    type        int         null,
+    status      int         not null,
+    constraint players_pk2
+        unique (id)
+);`,
+      `
+
+create table users
+(
+    id                       int auto_increment
+        primary key,
+    username                 varchar(50)  not null,
+    password                 varchar(100) null,
+    qq                       int          null,
+    primary_email            varchar(50)  null,
+    status                   int          not null,
+    register_date            datetime     not null,
+    last_login_date          datetime     null,
+    register_ip              varchar(50)  not null,
+    register_user_agent      varchar(255) null,
+    primary_premission_group int          null,
+    openid                   varchar(100) null
+);`
+    ]
   }
 ] as {
   version: string
--- src/router/Audit/submit.ts
+++ src/router/Audit/submit.ts
@@ -1,6 +1,9 @@
 import { Router } from 'express'
 import { queryOldPlayer } from '../../utils/queryOldPlayer'
 import type { Request } from '../../types/express'
+import { query } from '../../utils/db'
+import dayjs from 'dayjs'
+import { OkPacket } from 'mysql2'
 
 const router = Router()
 
@@ -14,6 +17,192 @@ router.post('/checkName', async (req: Request, res) => {
     })
   }
   res.send({ status: 200 })
+})
+
+const checkAudit = async (
+  user: number,
+  name?: string,
+  biliUsername?: string,
+  biliUid?: string
+): Promise<{
+  usable: boolean
+  msg?: string
+}> => {
+  // 有正在审核的
+  {
+    const [err, result] = await query`
+    select * from audits where user=${user} and status=1
+    `
+    if (err) {
+      return {
+        usable: false
+      }
+    }
+    if (result.length >= 1) {
+      return {
+        usable: false,
+        msg: '您已申请白名单，请等待审核'
+      }
+    }
+  }
+  // 是否已有白名单
+  {
+    const [err, result] = await query`
+    select * from players where user=${user}
+    `
+    if (err) {
+      return {
+        usable: false
+      }
+    }
+    if (result.length >= 1) {
+      return {
+        usable: false,
+        msg: '您已申请白名单，无法再次申请'
+      }
+    }
+  }
+  // 是否只检测是否已经申请
+  if (!name) {
+    return {
+      usable: true
+    }
+  }
+  // 未审核的 同名的
+  {
+    const [err, result] = await query`
+      select * from audits where name=${name} and status=1
+    `
+    if (err) {
+      return {
+        usable: false
+      }
+    }
+    if (result.length >= 1) {
+      return {
+        usable: false,
+        msg: '该id已申请白名单'
+      }
+    }
+  }
+  // bili信息重复 (排除审核未通过的)
+  {
+    const [err, result] = await query`
+    select * from audits where
+      (bili_uid=${biliUid} and status!=2) or 
+      (bili_username=${biliUsername} and status!=2)
+    `
+    if (err) {
+      return {
+        usable: false
+      }
+    }
+    if (result.length >= 1) {
+      return {
+        usable: false,
+        msg: '信息已被使用'
+      }
+    }
+  }
+}
+
+/**
+ * 初始化 用于验证是否可提交和交换配置项
+ */
+router.post('/init', async (req: Request, res) => {
+  const user = req.user
+  const check = await checkAudit(user.id)
+  if (check.usable == false) {
+    return res.send({
+      status: 403,
+      msg: check.msg
+    })
+  }
+  res.send({
+    status: 200
+  })
+})
+
+router.post('/', async (req: Request, res) => {
+  const { name, biliUsername, biliUid, screenshot } = req.body
+  const user = req.user
+  if (
+    !/^([a-zA-Z])([a-zA-Z0-9 ]{1,14})$/.test(name) ||
+    !/^.{1,20}$/.test(biliUsername) ||
+    !/^[1-9][0-9]{0,19}$/.test(biliUid) ||
+    !screenshot
+  ) {
+    return res.send({
+      status: 403
+    })
+  }
+  const [err, result] = await query<
+    {
+      qq: string
+    }[]
+  >`
+  select
+    *
+  from users
+  where id=${user.id}
+  `
+  if (err || result.length < 1) {
+    return res.send({
+      status: 403
+    })
+  }
+
+  const userinfo = result[0]
+
+  if (!userinfo.qq) {
+    return res.send({
+      status: 4031,
+      msg: '请先绑定QQ'
+    })
+  }
+
+  const player = await queryOldPlayer(userinfo.qq, name, req.user.id)
+  if (player.usable == false) {
+    return res.send({
+      status: 403,
+      msg: player.msg
+    })
+  }
+  const check = await checkAudit(user.id, name, biliUsername, biliUid)
+  if (check.usable == false) {
+    return res.send({
+      status: 403,
+      msg: check.msg
+    })
+  }
+  // 提交
+  {
+    const random = Math.floor(Math.random() * 100000)
+
+    const [err, result] = await query<OkPacket>`
+    insert into audits set ${{
+      id: dayjs().format(`YYYYMMDD${random}`),
+      user: user.id,
+      name,
+      bili_username: biliUsername,
+      bili_uid: biliUid,
+      screenshot,
+      status: 1,
+      date: new Date()
+    }};
+    `
+    if (err) {
+      return res.send({
+        status: 500
+      })
+    }
+    res.send({
+      status: 200,
+      data: {
+        insertId: result.insertId
+      }
+    })
+  }
 })
 
 export default router
