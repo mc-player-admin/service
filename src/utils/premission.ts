5f8315d1b0545004dd62d706785354d2ecf101ae
ltfei <ltfei@ltfei.cn>
2023-06-25 17:26:31 +0800

    feat(utils/premission): 封装 checkUserPremission

--- src/utils/premission.ts
+++ src/utils/premission.ts
@@ -49,36 +49,8 @@ export const auth = (premission: PartialSub<Premission>): RequestHandler => {
     for (let i in premission) {
       for (let j in premission[i as keyof typeof premission]) {
         const name = `${i}.${j}`
-        const [err, result] = await query<
-          {
-            value: boolean | null | 0 | 1
-          }[]
-        >`
-        select * from users
-        left join premission_group on 
-            primary_premission_group = premission_group.group and
-            premission_group.name=${name}
-        where users.id=${user.id}
-        `
-        if (err || result[0].value == 0) {
-          logger.info(
-            `[${req.path}] [${req.user.id}] 鉴权 ${JSON.stringify(
-              premission
-            )} ${name} 拒绝`
-          )
-          return res.send({
-            status: 403,
-            msg: '权限不足'
-          })
-        }
-        // 数据库没有约定权限
-        // @ts-ignore
-        if (result[0].value == null && !defaultPremission[i][j]) {
-          logger.info(
-            `[${req.path}] [${req.user.id}] 鉴权 ${JSON.stringify(
-              premission
-            )} ${name} 匹配默认权限拒绝`
-          )
+        const havePremission = await checkUserPremission(user.id, name)
+        if (!havePremission) {
           return res.send({
             status: 403,
             msg: '权限不足'
@@ -89,4 +61,32 @@ export const auth = (premission: PartialSub<Premission>): RequestHandler => {
     logger.info(`[${req.path}] [${req.user.id}] 鉴权 ${JSON.stringify(premission)} 通过`)
     next()
   }
+}
+
+/**
+ * 验证用户权限
+ */
+export const checkUserPremission = async (user: number, name: string) => {
+  const [err, result] = await query<
+    {
+      value: boolean | null | 0 | 1
+    }[]
+  >`
+  select * from users
+  left join premission_group on 
+      primary_premission_group = premission_group.group and
+      premission_group.name=${name}
+  where users.id=${user}
+  `
+  if (err || result[0].value == 0) {
+    logger.info(`[${user}] 鉴权 ${name} 拒绝`)
+    return false
+  }
+  // 数据库没有约定权限
+  // @ts-ignore
+  if (result[0].value == null && !defaultPremission[i][j]) {
+    logger.info(` [${user}] 鉴权  ${name} 匹配默认权限拒绝`)
+    return false
+  }
+  return true
 }
