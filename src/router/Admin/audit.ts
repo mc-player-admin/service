075cd06186810f01cab6e967ab103147f01c5591
ltfei <ltfei@ltfei.cn>
2023-06-28 13:26:35 +0800

    feat(Admin/audit): 审核

--- src/router/Admin/audit.ts
+++ src/router/Admin/audit.ts
@@ -1,10 +1,11 @@
 import { Router } from 'express'
+import type { Request } from '../../types/express'
 import { query } from '../../utils/db'
 
 const router = Router()
 
-router.post('/get', async (req, res) => {
-  const [err, result] = await query`
+const getList = () => {
+  return query`
   select
     audits.id,
     audits.user,
@@ -23,6 +24,10 @@ router.post('/get', async (req, res) => {
   from audits
   left join users on users.id=audits.user
   where audits.status=1`
+}
+
+router.post('/get', async (req, res) => {
+  const [err, result] = await getList()
   if (err) {
     return res.send({
       status: 500
@@ -31,6 +36,78 @@ router.post('/get', async (req, res) => {
   res.send({
     status: 200,
     data: result
+  })
+})
+
+router.post('/set', async (req: Request, res) => {
+  const user = req.user
+  const { id, approved, cause } = req.body
+
+  if (!approved && !cause) {
+    return res.send({
+      status: 403,
+      msg: '请填写原因'
+    })
+  }
+
+  // 查询
+  const [err, result] = await query<
+    {
+      id: string
+      user: number
+      name: string
+      bili_username: string
+      bili_uid: string
+      screenshot: string
+      status: number
+      result?: any
+      cause?: any
+      approver?: any
+      date: string
+    }[]
+  >`select * from audits where id=${id} and status=1`
+  if (err) {
+    return res.send({
+      status: 500
+    })
+  }
+  if (result.length < 1) {
+    return res.send({
+      status: 403,
+      msg: '未找到id'
+    })
+  }
+  const audit = result[0]
+
+  // 修改审核表
+  {
+    const [err] = await query`update audits set ${{
+      status: 2,
+      result: approved ? 1 : 2,
+      cause,
+      approver: user.id
+    }} where id=${id}`
+
+    if (err) {
+      return res.send({
+        status: 500
+      })
+    }
+  }
+  // 添加玩家
+  {
+    if (approved) {
+      query`insert into players set ${{
+        name: audit.name,
+        user: audit.user,
+        type: 1,
+        status: 1
+      }}`
+    }
+  }
+  // todo: 添加白名单
+  res.send({
+    status: 200
   })
 })
 
